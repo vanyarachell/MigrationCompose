52216eb20439007eb8992a8c58373ba1fa9783d0
Vanya Rachel <vanyarachel05@gmail.com>
2023-12-31 20:23:54 +1000

    Initial commit

---
+++ app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.samples.apps.sunflower.plantdetail
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.google.samples.apps.sunflower.R
+import com.google.samples.apps.sunflower.data.Plant
+import com.google.samples.apps.sunflower.viewmodels.PlantDetailViewModel
+
+@Composable // Stateful : opinionated
+fun PlantDetailDescription(plantDetailViewModel: PlantDetailViewModel) {
+    val currentPlant by plantDetailViewModel.plant.observeAsState()
+    currentPlant?.let { plant ->
+        PlantDetailDescription(plant)
+    }
+}
+
+@Composable // Stateless : Preview + reusable
+private fun PlantDetailDescription(plant: Plant) {
+    Surface {
+        PlantName(name = plant.name)
+    }
+}
+
+@Composable
+fun PlantName(name: String) {
+    Text(
+        text = name,
+        modifier = Modifier
+            .padding(horizontal = dimensionResource(id = R.dimen.margin_small))
+            .fillMaxWidth()
+            .wrapContentWidth(Alignment.CenterHorizontally),
+        style = MaterialTheme.typography.h5
+    )
+}
+
+@Preview
+@Composable
+fun PlantNamePreview() {
+    MaterialTheme {
+        PlantName(name = "Avocado")
+    }
+}
